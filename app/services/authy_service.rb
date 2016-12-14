fbb70d5e9d2e013420103f730c6b90ac085a12be
Matt D <MDes41@users.noreply.github.com>
2016-12-13 20:27:15 -0700

    Refactor to authy service

--- app/services/authy_service.rb
+++ app/services/authy_service.rb
@@ -1,12 +1,34 @@
 class AuthyService
 
+	def initialize
+		@conn = Faraday.new(:url => 'https://api.authy.com')
+		@authy_params = { api_key: ENV['authy_token'], country_code: '1' }
+	end
+
   def self.send_verification_code(cellphone)
-    Faraday.post("https://api.authy.com/protected/json/phones/verification/start?api_key=#{ENV['authy_token']}&via=sms&phone_number=#{cellphone}&country_code=1")
+  	new.send_verification_code_response(cellphone)
   end
 
+	def send_verification_code_response(cellphone)
+		conn.post do |req|
+		  req.url '/protected/json/phones/verification/start'
+		  req.params = { phone_number: cellphone, via: 'sms'}.merge(authy_params)
+		end
+	end
+
   def self.check_verification_code(user, verification_code)
-    response = Faraday.get("https://api.authy.com/protected/json/phones/verification/check?api_key=#{ENV['authy_token']}&country_code=1&phone_number=#{user.cellphone}&verification_code=#{verification_code}")
+  	response = new.check_verification_code_response(user, verification_code)
     JSON.parse(response.body)["message"]
   end
 
+  def check_verification_code_response(user, verification_code)
+  	conn.get do |req|
+		  req.url '/protected/json/phones/verification/check'
+		  req.params = { verification_code: verification_code, phone_number: user.cellphone }.merge(authy_params)
+		end
+  end
+
+  private
+  	attr_reader :conn, :authy_params
+  	
 end
