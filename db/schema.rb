4467c3999150092a785dfbdbfcdafb0a7d073ace
Dave Junta <djunta@gmail.com>
2016-12-14 14:43:13 -0700

    to merge

--- db/schema.rb
+++ db/schema.rb
@@ -18,10 +18,9 @@ ActiveRecord::Schema.define(version: 20161214014559) do
 
   create_table "users", force: :cascade do |t|
     t.citext   "username"
-    t.integer  "role",            default: 0
     t.string   "password_digest"
     t.citext   "email"
-    t.string   "cellphone"
+    t.string   "sms_number"
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
     t.integer  "status",          default: 0
