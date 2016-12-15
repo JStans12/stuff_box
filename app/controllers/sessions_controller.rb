bcec6528c706932acb0ccb42d2d44d4736e17849
Joey Stansfield <joseph.r.stansfield@gmail.com>
2016-12-15 10:40:45 -0700

    share logic with create and login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,7 @@ class SessionsController < ApplicationController
   def login
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
-      response = AuthyService.send_verification_code(user.cellphone)
-      session[:user_id] = user.id
-      redirect_to verify_phone_path
+      continue_to_phone_verification(user)
     else
       flash.now[:danger] = "Unable to login"
       render :new
@@ -18,9 +16,7 @@ class SessionsController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      response = AuthyService.send_verification_code(user.cellphone)
-      session[:user_id] = user.id
-      redirect_to verify_phone_path
+      continue_to_phone_verification(user)
     else
       flash.now[:danger] = "Unable to create account"
       render :new
@@ -52,5 +48,11 @@ class SessionsController < ApplicationController
 
     def user_params
       params.permit(:username, :email, :cellphone, :password, :password_confirmation)
+    end
+
+    def continue_to_phone_verification(user)
+      response = AuthyService.send_verification_code(user.cellphone)
+      session[:user_id] = user.id
+      redirect_to verify_phone_path
     end
 end
