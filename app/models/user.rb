67f765f9d4a2152173a5d93eeb3ee95630ea1064
Joey Stansfield <joseph.r.stansfield@gmail.com>
2016-12-14 18:51:13 -0700

    folders general structure setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
+  after_create :create_root_folder
+
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :cellphone, presence: true
@@ -8,4 +10,20 @@ class User < ApplicationRecord
 
   enum role: [:admin, :user]
   enum status: [:pending, :confirmed]
+
+  has_many :user_folders
+  has_many :folders, through: :user_folders, dependent: :destroy
+
+  def create_root_folder
+    root = folders.create(name: "root")
+    user_folders.create(folder_id: root.id, permissions: 0)
+    update(root: root.id)
+    save
+  end
+
+  def new_folder(name, parent_id = root)
+    parent = Folder.find(parent_id)
+    folder = folders.create(name: name, parent_id: parent.id)
+    user_folders.create(folder_id: folder.id, permissions: 0)
+  end
 end
---
+++ db/migrate/20161214234011_create_user_folders.rb
@@ -0,0 +1,11 @@
+class CreateUserFolders < ActiveRecord::Migration[5.0]
+  def change
+    create_table :user_folders do |t|
+      t.integer :permissions
+      t.references :user, foreign_key: true
+      t.references :folder, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
