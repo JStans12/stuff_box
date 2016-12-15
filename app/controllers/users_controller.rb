1e76dfb3b231ac9a81f05ba0c56283708ded3189
Joey Stansfield <joseph.r.stansfield@gmail.com>
2016-12-15 11:01:32 -0700

    changes routes for better flow when updating password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
 
+  def update
+  end
+
   def update_password
-    
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   root "dashboard#index"
 
-  get "/login",           to: "sessions#new"
-  post "/login",          to: "sessions#login"
-  post "/create_account", to: "sessions#create"
-  get "/logout",					to: "sessions#destroy"
-  get "/verify_phone",    to: "sessions#verify_phone"
-  post "/verify_phone",   to: "sessions#verify_user"
-  get "/update_password",  to: "users#update_password"
+  get "/login",            to: "sessions#new"
+  post "/login",           to: "sessions#login"
+  post "/create_account",  to: "sessions#create"
+  get "/logout",					 to: "sessions#destroy"
+  get "/verify_phone",     to: "sessions#verify_phone"
+  post "/verify_phone",    to: "sessions#verify_user"
+  get "/update_password",  to: "users#update"
+  post "/update_password", to: "users#update_password"
 end
