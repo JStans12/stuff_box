24ac79970fd296f5a6caeefc19d3709fd0f021e7
Joey Stansfield <joseph.r.stansfield@gmail.com>
2016-12-15 12:39:39 -0700

    password reset fully working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,30 @@ class UsersController < ApplicationController
   end
 
   def update_password
+    user = User.find_by(username: params[:username])
+    if user.authenticate(params[:current_password])
+      response = AuthyService.send_verification_code(user.cellphone)
+      session[:user_id] = user.id
+      session[:new_password] = params[:new_password]
+      redirect_to update_password_verify_phone_path
+    else
+      # error
+    end
   end
 
+  def verify_phone
+  end
+
+  def verify_user
+    message = AuthyService.check_verification_code(current_user, params[:verification_code])
+    if message == "Verification code is correct."
+      current_user.password = session[:new_password]
+      current_user.save
+      flash[:success] = "Password for #{current_user.username.titleize} successfully changed!"
+      redirect_to root_path
+    else
+      flash.now[:danger] = "Please enter your validation code again, or log in to request another code."
+      render :verify_phone
+    end
+  end
 end
