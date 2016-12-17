dcabb349bc8ff70222ec35e52ec334b9560d97b0
Dave Junta <djunta@gmail.com>
2016-12-16 21:14:37 -0700

    Adds border to carousel pics and login links to carousel buttons

--- spec/features/user_visits_login_page_spec.rb
+++ spec/features/user_visits_login_page_spec.rb
@@ -50,4 +50,35 @@ RSpec.feature "User visits login page" do
     end
   end
 
+  context "they can't create a new user" do
+    it "with a password that already exists" do
+      user = User.create(username: "John Elway", password: "je", password_confirmation: "je", email: 'je@je.com', cellphone: '1234561234')
+
+      visit '/login'
+
+      within('#login') do
+        fill_in "Username", with: user.username
+        fill_in "Password", with: user.password
+
+        click_button "Log In"
+      end
+
+      click_on "Logout"
+      click_on "Login"
+      within('#create-user') do
+        fill_in "Username", with: "John Elway"
+        require "pry"; binding.pry
+        fill_in "Email", with: "je@je.com"
+        fill_in "Cell Number", with: '3031234567'
+        fill_in "Password", with: 'p'
+        fill_in "Confirm Password", with: 'p'
+
+        click_button "Create Account"
+      end
+
+      expect(page).to have_content('Unable to login')
+    end
+  end
+
+
 end
