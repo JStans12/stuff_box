7000668d6939639142e068b7febbedcfcbbe67af
Joey Stansfield <joseph.r.stansfield@gmail.com>
2016-12-17 17:22:07 -0700

    added visibility to folders and query method

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161215011221) do
+ActiveRecord::Schema.define(version: 20161217235102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,8 +19,9 @@ ActiveRecord::Schema.define(version: 20161215011221) do
   create_table "folders", force: :cascade do |t|
     t.string   "name"
     t.integer  "parent_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "visibility", default: 0
   end
 
   create_table "user_folders", force: :cascade do |t|
