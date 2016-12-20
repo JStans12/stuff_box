286b2fa7aec1c12d0bbbe98ec255bfeac3f6c8ce
Nicholas Martinez <nicholasmartinez@gmail.com>
2016-12-20 08:33:18 -0700

    tested delete buttons appear

---
+++ spec/features/user/user_can_download_file_spec.rb
@@ -0,0 +1,40 @@
+require "rails_helper"
+
+RSpec.feature "User downloads" do
+  context "a user can download a file from their folder" do
+    it "can download files" do
+      user = User.create(username: "Madonna", password: "m", password_confirmation: "m", email: 'm@m.com', cellphone: '3038857241')
+      upload = Upload.create(url: "https://s3-us-west-1.amazonaws.com/stuff-box/Retest.png", name: "Retest", folder_id: user.root_folder.id )
+
+      user.confirmed!
+      visit '/login'
+
+
+      within('#login') do
+        fill_in "Username", with: user.username
+        fill_in "Password", with: user.password
+        click_button "Log In"
+      end
+
+    expect(page).to have_button("Download")
+    end
+  end
+  context "a user can delete a file from their folder" do
+    it "can delete files" do
+      user = User.create(username: "Madonna", password: "m", password_confirmation: "m", email: 'm@m.com', cellphone: '3038857241')
+      upload = Upload.create(url: "https://s3-us-west-1.amazonaws.com/stuff-box/Retest.png", name: "Retest", folder_id: user.root_folder.id )
+
+      user.confirmed!
+      visit '/login'
+
+
+      within('#login') do
+        fill_in "Username", with: user.username
+        fill_in "Password", with: user.password
+        click_button "Log In"
+      end
+
+    expect(page).to have_button("Delete")
+    end
+  end
+end
