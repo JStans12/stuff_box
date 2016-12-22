226df59146a1500d28eaaeaa58edfc107634842e
Nicholas Martinez <nicholasmartinez@gmail.com>
2016-12-22 09:32:14 -0700

    added dependant destroy to comment on uploads

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -2,7 +2,7 @@ class Upload < ApplicationRecord
   attr_reader :s3
   belongs_to :folder
 
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   def s3
     AWS::S3::Client.new(:access_key_id => ENV['AWS_ACCESS_KEY_ID'], :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'])
