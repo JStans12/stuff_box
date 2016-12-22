ba8897d2d49cdd619b6a8e1aca22551d724aa184
Nicholas Martinez <nicholasmartinez@gmail.com>
2016-12-21 17:01:21 -0700

    download refactored

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -5,15 +5,16 @@ class Upload < ApplicationRecord
   has_many :comments
 
   def s3
-    AWS::S3.new(:access_key_id => ENV['AWS_ACCESS_KEY_ID'], :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'])
-  end
-
-  def bucket
-    s3.buckets['stuff-box']
+    AWS::S3::Client.new(:access_key_id => ENV['AWS_ACCESS_KEY_ID'], :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'])
   end
 
   def save_file(file)
+    name = file.name
     path = File.expand_path("~/Downloads")
-    download = File.open("#{Rails.root}/tmp/#{file.name}", 'wb').write(bucket.objects[file.name].read)
+    File.open("#{Rails.root}/tmp/#{name}", 'wb') do |file|
+      s3.get_object({ bucket_name: 'stuff-box', key: name, target: "tmp/#{name}" }) do |chunk|
+        file.write(chunk)
+      end
+    end
   end
 end
